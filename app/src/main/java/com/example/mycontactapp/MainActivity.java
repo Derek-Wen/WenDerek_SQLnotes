cc904274029f0b2928693c5e4ec6e1d002259391
Derek-Wen <derek.hz.wen@gmail.com>
2019-05-30 13:45:27 -0700

    Changed

--- app/src/main/java/com/example/mycontactapp/MainActivity.java
+++ app/src/main/java/com/example/mycontactapp/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.mycontactapp;
 
-import android.content.Intent;
 import android.database.Cursor;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -17,94 +16,130 @@ public class MainActivity extends AppCompatActivity {
     EditText editNumber;
     EditText editAddress;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d("MyContactApp", "MainActivity: setting up the layout");
         setContentView(R.layout.activity_main);
 
         editName = findViewById(R.id.editText_name);
         editNumber = findViewById(R.id.editText_phone);
         editAddress = findViewById(R.id.editText_address);
 
-        myDb = new DatabaseHelper( this);
-        Log.d("MyContactApp", "Main activity: Instantiated Database Helper");
+        myDb = new DatabaseHelper(this );
+        Log.d("MyContactApp", "MainActivity: instantiated DatabaseHelper");
     }
 
-    public void addData(View view) {
+    public void addData(View view){
 
-        boolean isInserted = myDb.insertData(editName.getText().toString(), editNumber.getText().toString(),
-                editAddress.getText().toString());
+        boolean nameInserted = myDb.insertContact(editName.getText().toString(), editNumber.getText().toString(), editAddress.getText().toString());
 
-        if (isInserted) {
+        if (nameInserted){
             Toast.makeText(MainActivity.this, "Success - contact inserted", Toast.LENGTH_LONG).show();
         }
-        else {
+        else{
             Toast.makeText(MainActivity.this, "FAILED - contact not inserted", Toast.LENGTH_LONG).show();
+
         }
     }
 
-    public void viewData(View view)
-    {
-        Log.d("MyContactApp", "MainActivity: View Contact Button Pressed");
+    public void viewData(View view){
         Cursor res = myDb.getAllData();
 
-        if (res.getCount() == 0) {
-            showMessage("Error",  "no data found in database");
+        if (res.getCount() == 0){
+            showMessage("Error", "No data found in database");
+            return;
         }
 
         StringBuffer buffer = new StringBuffer();
-        while (res.moveToNext()) {
-            buffer.append("ID: " + res.getString(0) + "\n" +
-                    "Name: " + res.getString(1) + "\n" +
-                    "Phone number: " + res.getString(2) + "\n" +
-                    "House address: " + res.getString(3) + "\n\n");
+        while (res.moveToNext()){
+            //append res columns 0, ... to the buffer
+            buffer.append("ID: " + res.getString(0) +"\n");
+            buffer.append("Name: " + res.getString(1) + "\n");
+            buffer.append("Phone Number: " +res.getString(2) + "\n");
+            buffer.append("Address: " + res.getString(3) + "\n");
         }
-
-        Log.d("MyContactApp", "MainActivity: In viewData - Buffer assembled");
 
         showMessage("Data", buffer.toString());
     }
 
-    public void showMessage(String title, String message) {
-        Log.d("MyContactApp", "MainActivity:  showMessage - building alert dialog");
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+    public void searchData(View view) {
+        Cursor res = myDb.getAllData();
 
+        if (res.getCount() == 0) {
+            showMessage("Error", "No data found in database");
+            return;
+        }
+
+        StringBuffer buffer = new StringBuffer();
+        while (res.moveToNext()) {
+            if (!editName.getText().toString().equals("") && editNumber.getText().toString().equals("") && editAddress.getText().toString().equals("")) {// name ONLY
+                if (res.getString(1).equals(editName.getText().toString())) {
+                    buffer.append("ID: " + res.getString(0) + "\n");
+                    buffer.append("Name: " + res.getString(1) + "\n");
+                    buffer.append("Phone Number: " + res.getString(2) + "\n");
+                    buffer.append("Address: " + res.getString(3) + "\n");
+                }
+            }
+            else if (editName.getText().toString().equals("") && !editNumber.getText().toString().equals("") && editAddress.getText().toString().equals("")){//number ONLY
+                if (res.getString(2).equals(editNumber.getText().toString())) {
+                    buffer.append("ID: " + res.getString(0) + "\n");
+                    buffer.append("Name: " + res.getString(1) + "\n");
+                    buffer.append("Phone Number: " + res.getString(2) + "\n");
+                    buffer.append("Address: " + res.getString(3) + "\n");
+                }
+            }
+            else if (editName.getText().toString().equals("") && editNumber.getText().toString().equals("") && !editAddress.getText().toString().equals("")){ // address ONLY
+                if (res.getString(3).equals(editAddress.getText().toString())) {
+                    buffer.append("ID: " + res.getString(0) + "\n");
+                    buffer.append("Name: " + res.getString(1) + "\n");
+                    buffer.append("Phone Number: " + res.getString(2) + "\n");
+                    buffer.append("Address: " + res.getString(3) + "\n");
+                }
+            }
+            else if (!editName.getText().toString().equals("") && !editNumber.getText().toString().equals("") && editAddress.getText().toString().equals("")){//name and number ONLY
+                if (res.getString(1).equals(editName.getText().toString()) && res.getString(2).equals(editNumber.getText().toString())) {
+                    buffer.append("ID: " + res.getString(0) + "\n");
+                    buffer.append("Name: " + res.getString(1) + "\n");
+                    buffer.append("Phone Number: " + res.getString(2) + "\n");
+                    buffer.append("Address: " + res.getString(3) + "\n");
+                }
+            }
+            else if (!editName.getText().toString().equals("") && editNumber.getText().toString().equals("") && !editAddress.getText().toString().equals("")){//name and address ONLY
+                if (res.getString(1).equals(editName.getText().toString()) && res.getString(3).equals(editAddress.getText().toString())){
+                    buffer.append("ID: " + res.getString(0) + "\n");
+                    buffer.append("Name: " + res.getString(1) + "\n");
+                    buffer.append("Phone Number: " + res.getString(2) + "\n");
+                    buffer.append("Address: " + res.getString(3) + "\n");
+                }
+            }
+            else if (editName.getText().toString().equals("") && !editNumber.getText().toString().equals("") && !editAddress.getText().toString().equals("")){//number and address ONLY
+                if (res.getString(2).equals(editNumber.getText().toString()) && res.getString(3).equals(editAddress.getText().toString())){
+                    buffer.append("ID: " + res.getString(0) + "\n");
+                    buffer.append("Name: " + res.getString(1) + "\n");
+                    buffer.append("Phone Number: " + res.getString(2) + "\n");
+                    buffer.append("Address: " + res.getString(3) + "\n");
+                }
+            }
+            else {
+                if (res.getString(1).equals(editName.getText().toString()) &&
+                        res.getString(2).equals(editNumber.getText().toString()) &&
+                        res.getString(3).equals(editAddress.getText().toString())) {
+                    buffer.append("ID: " + res.getString(0) + "\n");
+                    buffer.append("Name: " + res.getString(1) + "\n");
+                    buffer.append("Phone Number: " + res.getString(2) + "\n");
+                    buffer.append("Address: " + res.getString(3) + "\n");
+                }
+            }
+        }
+
+        showMessage("No results found", buffer.toString());
+    }
+
+    public void showMessage(String title, String message){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setCancelable(true);
         builder.setTitle(title);
         builder.setMessage(message);
         builder.show();
-    }
-
-    public static final String EXTRA_NAME = "com.example.mycontactapp";
-
-    public void searchRecord(View view) {
-        Log.d("MyContactApp", "MainActivity: launching search");
-        Cursor curs = myDb.getAllData();
-        StringBuffer buffer = new StringBuffer();
-        if (editName.getText().toString().isEmpty() && editNumber.getText().toString().isEmpty()
-                && editAddress.getText().toString().isEmpty()) {
-            showMessage("No results found", "None of your contacts matches your search parameter");
-            return;
-        }
-
-        while (curs.moveToNext()){
-            if ((editName.getText().toString().isEmpty() || editName.getText().toString().equals(curs.getString(1)))
-                    && (editNumber.getText().toString().isEmpty() || editNumber.getText().toString().equals(curs.getString(2)))
-                    && (editAddress.getText().toString().isEmpty() || editAddress.getText().toString().equals(curs.getString(3))))
-            {
-                buffer.append("ID: " + curs.getString(0) + "\n" +
-                        "Name - " + curs.getString(1) + "\n" +
-                        "Phone number - " + curs.getString(2) + "\n" +
-                        "House address - " + curs.getString(3) + "\n\n");
-            }
-        }
-
-        if (buffer.toString().isEmpty()) {
-            showMessage("Error", "No matches found");
-            return;
-        }
-        showMessage("Search results", buffer.toString());
     }
 }
